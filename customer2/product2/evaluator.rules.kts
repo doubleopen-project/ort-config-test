1708e8b2a7cb3b2d36da89675e3f5f3d0d6b0a1d
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-06-04 16:05:39 +0300

    Add evaluator rules for customers and products
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ customer2/product2/evaluator.rules.kts
@@ -0,0 +1,10 @@
+// Add an example RuleViolation to the list
+ruleViolations += RuleViolation(
+    rule = "TEST_RULE",
+    pkg = null,
+    license = null,
+    licenseSource = null,
+    severity = Severity.ERROR,
+    message = "This is an example RuleViolation for test cases.",
+    howToFix = ""
+)
